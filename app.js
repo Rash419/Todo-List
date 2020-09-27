48683a977bc71d7d61d3482a03a15056ebc8ea5a
Rash419 <rasheshpadia419@gmail.com>
2020-09-27 16:46:18 +0530

    added delete and update feature

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 //mongodb+srv://admin:<password>@cluster0.inyjd.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority
 var mongoDB = 'mongodb+srv://admin:admin123@cluster0.inyjd.gcp.mongodb.net/TodoList?retryWrites=true&w=majority'
@@ -18,11 +19,11 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use('/', indexRouter);
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,18 @@ var todoTaskModel = require('../models/TodoTask');
 /* GET home page. */
 
 router.get('/', function (req, res, next) {
-  res.render('index');
+  todoTaskModel.find({},(err,tasks) => {
+    if(err) { return next(err) };
+    res.render('index',{todotask : tasks});
+  });
 });
 
 router.post('/addTask', async (req, res, next) => {
+  console.log(req.body);
   const todoTask = new todoTaskModel({
-    content: req.body.content
+    content: req.body.content,
   });
-  console.log(req.body.content);
+
   try {
     await todoTask.save();
     res.redirect('/');
@@ -20,5 +24,30 @@ router.post('/addTask', async (req, res, next) => {
     res.redirect('/');
   }
 });
+
+router.get("/edit/:id",(req,res,next) => {
+  const id = req.params.id;
+  todoTaskModel.find({},function(err,tasks){
+    if(err) {return next(err)}
+    res.render("indexEdit",{todotask : tasks,idTask : id});
+  })
+})
+
+router.post("/edit/:id",(req,res,next) => {
+  todoTaskModel.findByIdAndUpdate(req.params.id,{content : req.body.content}, err => {
+    if(err) {return next(err)}
+    res.redirect("/");
+  })
+})
+
+router.get("/remove/:id",(req,res,next) => {
+  todoTaskModel.findByIdAndRemove(req.params.id, err=> {
+    if(err) {return next(err)}
+    res.redirect("/");
+  })
+})
+/*router.post('/addTask',function(req,res,next){
+  console.log('post method');
+})*/
 
 module.exports = router;
